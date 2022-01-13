a28beaa0a81fa480027a8326f7bdd7a04d7e717b
bvldog <bvldog@gmail.com>
2022-01-13 13:13:39 +0900

    DHT22 test

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,19 @@
 #include <I2C_Scan.h>
 #include <settings.h>
 #include <SPIFFS.h>
-#include <dht22_reading.h>
+#include <DHT.h>
+// #include <dht22_reading.h>
 
-// void refresh_reading();
-// void I2C_Scan();
+void refresh_reading();
+void I2C_Scan();
 
 Adafruit_BME280 bme; //I2C
 TwoWire I2CBME = TwoWire(0);
+
+#define DHTPIN 22
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE); //DHT22 Define
 
 TFT_eSPI tft = TFT_eSPI();
 
@@ -32,7 +38,7 @@ void setup()
 
   Wire.begin();
   Wire.beginTransmission(BME280_Addr);
-  // I2C_Scan();
+  I2C_Scan();
   tft.init();
 
   tft.setTextColor(fg, bg);
@@ -60,6 +66,7 @@ void setup()
     Serial.println("BME280 Sensor Found");
     Serial.print("TFT_LCD Loaded!");
   }
+  dht.begin();
 
   tft.fillScreen(bg); // Clear the Screen
 }
@@ -69,4 +76,36 @@ void loop()
   // put your main code here, to run repeatedly:
   refresh_reading(&bme, &tft);
   I2C_Scan();
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f))
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
 }
